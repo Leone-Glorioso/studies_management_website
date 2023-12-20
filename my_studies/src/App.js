bb3ac7eebfab3a39632f8197aae56f5f05e2eced
Konstantinos Leontiadis <123456kost@gmail.com>
2023-12-20 15:24:40 +0200

    Currently not working/ will have to fix later

--- my_studies/src/App.js
+++ my_studies/src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginWindow from "./Start_Page/LoginWindow";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginWindow />} />
+      </Routes>
+    </Router>
 }
 
 export default App;
---
+++ my_studies/src/Start_Page/LoginWindow.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import firebase from "firebase/compat";
+
+const LoginWindow = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const userCredential = await firebase.auth().signInWithEmailAndPassword(username, password);
+            const user = userCredential.user;
+            console.log('Login successful:', user);
+            // Add your further logic after successful login
+        } catch (error) {
+            console.error('Login failed:', error.message);
+            // Handle login error here
+        }
+    };
+
+    return (
+        <div
+            style={{
+                backgroundColor: 'beige',
+                padding: '20px',
+                width: '300px',
+                margin: 'auto',
+                marginTop: '100px',
+                borderRadius: '10px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+            }}
+        >
+            <h2>Login</h2>
+            <label>
+                Username:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </label>
+            <br />
+            <button
+                style={{
+                    backgroundColor: 'forestgreen',
+                    color: 'white',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                }}
+                onClick={handleLogin}
+            >
+                Login
+            </button>
+        </div>
+    );
+};
+
+export default LoginWindow;
