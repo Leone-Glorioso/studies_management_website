4271257376162ecac25e8c8498ab347dbf9fa97d
Konstantinos Leontiadis <123456kost@gmail.com>
2023-12-20 18:28:33 +0200

    Changed Things For Testing Purposes

--- my_studies/src/App.js
+++ my_studies/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import LoginWindow from "./components/Start_Page/LoginWindow";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-    <Router>
-      <Routes>
-        <Route path='/' element={<LoginWindow />} />
-      </Routes>
-    </Router>
+    return(
+        <Router>
+          <Routes>
+              <Route path='/navbar' element={<Navbar/>} />
+              <Route path='/' element={<Sidebar/>} />
+          </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ my_studies/src/components/Auth/AuthContext.js
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import Cookies from 'universal-cookie';
+
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const cookies = new Cookies();
+
+    const userIsAuthenticated = () => {
+        return cookies.get('user') !== undefined;
+    };
+
+
+    const userLogin = (user, accessToken) => {
+        sessionStorage.setItem("user", JSON.stringify({ user, accessToken }));
+        cookies.set('user', JSON.stringify({ user, accessToken }), {
+            expires: new Date(Date.now() + 1000000)
+        })
+        setUser({ user, accessToken });
+    };
+
+
+    const getUser = () => {
+        return cookies.get('user')
+    }
+
+
+    const userLogout = () => {
+        // Clear the user data from local storage
+        sessionStorage.removeItem("user");
+        cookies.remove('user')
+        setUser(null);
+    };
+
+    const contextValue = {
+        user,
+        userIsAuthenticated,
+        userLogin,
+        userLogout,
+        getUser,
+    };
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContext;
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+export { AuthProvider };
