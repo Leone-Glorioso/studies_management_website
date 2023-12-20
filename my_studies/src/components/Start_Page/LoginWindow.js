af1ba6946ec808f2e2315a1e1998a8dce19f470c
Konstantinos Leontiadis <123456kost@gmail.com>
2023-12-21 00:02:23 +0200

    Very Minor attempts at improvement

--- my_studies/src/components/Start_Page/LoginWindow.js
+++ my_studies/src/components/Start_Page/LoginWindow.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./LoginWindow.css";
-import { FaUser, FaLock } from 'react-icons/fa';
+import { FaUser, FaLock, FaUnlockAlt } from 'react-icons/fa';
+import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
 
 const LoginWindow = (props) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [emailError, setEmailError] = useState("")
     const [passwordError, setPasswordError] = useState("")
+    const [isTooltipVisible, setTooltipVisible] = useState(false);
+
+    const handleMouseEnter = () => {
+        setTooltipVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setTooltipVisible(false);
+    };
 
     const navigate = useNavigate();
 
@@ -15,51 +27,79 @@ const LoginWindow = (props) => {
         // You'll update this function later...
     }
 
-        return (
-            <div className={"mainContainer"}>
-                <div className="inputContainer">
-                    <div className="input-group">
-                    <span className="icon">
-                      <FaUser />
-                    </span>
-                        <input
-                            value={email}
-                            placeholder="Όνομα Χρήστη"
-                            onChange={ev => setEmail(ev.target.value)}
-                            className="inputBox"
-                        />
-                    </div>
-                    <label className="errorLabel">{emailError}</label>
-                </div>
-                <br />
-                <div className="inputContainer">
-                    <div className="input-group">
-                        <span className="icon">
-                          <FaLock />
-                        </span>
-                        <input
-                            value={password}
-                            placeholder="Κωδικός"
-                            type="password" // Ensure the input type is set to "password" for password fields
-                            onChange={ev => setPassword(ev.target.value)}
-                            className="inputBox"
-                        />
-                    </div>
-                    <label className="errorLabel">{passwordError}</label>
-                </div>
-                <br />
-                <div className={"container"}>
-                    <button className="round-button">
-                        <span className="lock-icon">&#128274;</span>
-                    </button>
-                    <input
-                        className={"green-button"}
-                        type="button"
-                        onClick={onButtonClick}
-                        value={"Σύνδεση"} />
-                </div>
-            </div>
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
 
+        return (
+            <div>
+                <div className={"mainContainer"}>
+                    <div className="inputContainer">
+                        <div className="input-group">
+                        <span className="icon">
+                          <FaUser />
+                        </span>
+                            <input
+                                value={email}
+                                placeholder="Όνομα Χρήστη"
+                                onChange={ev => setEmail(ev.target.value)}
+                                className="inputBox"
+                            />
+                        </div>
+                        <label className="errorLabel">{emailError}</label>
+                    </div>
+                    <br />
+                    <div className="inputContainer">
+                        <div className="input-group">
+                            <span className="icon">
+                              <FaLock />
+                            </span>
+                            <input
+                                value={password}
+                                placeholder="Κωδικός"
+                                type="password" // Ensure the input type is set to "password" for password fields
+                                onChange={ev => setPassword(ev.target.value)}
+                                className="inputBox"
+                            />
+                        </div>
+                        <label className="errorLabel">{passwordError}</label>
+                    </div>
+                    <br />
+                    <div className={"container"}>
+                        <button className="round-button">
+                            <span className="lock-icon">
+                              <FaUnlockAlt />
+                            </span>
+                        </button>
+                        {isTooltipVisible && <div className="tooltip"><p>This is pop-up text</p></div>}
+                        <input
+                            className={"green-button"}
+                            type="button"
+                            onClick={onButtonClick}
+                            value={"Σύνδεση"} />
+                    </div>
+                </div>
+                {/*<div className="carousel-container">*/}
+                {/*    <Slider {...settings}>*/}
+                {/*        <div className="carousel-slide">*/}
+                {/*            <h3>Slide 1</h3>*/}
+                {/*            <p>This is some text for slide 1. Add your content here.</p>*/}
+                {/*        </div>*/}
+                {/*        <div className="carousel-slide">*/}
+                {/*            <h3>Slide 2</h3>*/}
+                {/*            <p>This is some text for slide 2. Add your content here.</p>*/}
+                {/*        </div>*/}
+                {/*        <div className="carousel-slide">*/}
+                {/*            <h3>Slide 3</h3>*/}
+                {/*            <p>This is some text for slide 3. Add your content here.</p>*/}
+                {/*        </div>*/}
+                {/*    </Slider>*/}
+                {/*</div>*/}
+            </div>
         );
 
 };
