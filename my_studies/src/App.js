79f467209d499439a0e08ed0bb6c0504dec98ec4
Konstantinos Leontiadis <123456kost@gmail.com>
2023-12-24 19:34:02 +0200

    Trying to configure Figma To React part1

--- my_studies/src/App.js
+++ my_studies/src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import StartPage from "./components/Start_Page/StartPage";
+import Teacher_Sidebar from "./components/Teacher_Page_Staples/Teacher_Sidebar";
 
 function App() {
     return(
@@ -15,6 +16,7 @@ function App() {
               <Route path='/' element={<Sidebar/>} />
               <Route path='/login' element={<LoginWindow/>}/>
               <Route path='/start' element={<StartPage/>}/>
+              <Route path='/ts' element={<Teacher_Sidebar/>}/>
           </Routes>
         </Router>
     );
