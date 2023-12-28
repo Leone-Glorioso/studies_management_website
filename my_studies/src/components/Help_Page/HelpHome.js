1ac8efe44dd9ef6d6ed8ff35676a7800958b32ae
Konstantinos Leontiadis <123456kost@gmail.com>
2023-12-28 20:24:06 +0200

    Home Page styled not functional

---
+++ my_studies/src/components/Help_Page/HelpHome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelpNavbar from "./HelpNavbar";
+import HelpHomeBody from "./HelpHomeBody";
+
+function HelpHome() {
+    return (
+        <>
+            <HelpNavbar/>
+            <HelpHomeBody/>
+        </>
+    );
+}
+
+export default HelpHome;
