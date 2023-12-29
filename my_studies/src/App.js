469c77d93f486f79a8401740905e7ad0db39fde4
Konstantinos Leontiadis <123456kost@gmail.com>
2023-12-29 14:09:10 +0200

    Problems with accordions

--- my_studies/src/App.js
+++ my_studies/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginWindow from "./components/Start_Page/LoginWindow";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
@@ -7,8 +6,9 @@ import Sidebar from "./components/Student_Home/Sidebar";
 import StudentCert from "./components/Student_Certificates/StudentCert";
 import StartPage from "./components/Start_Page/StartPage";
 import Teacher_Sidebar from "./components/Teacher_Page_Staples/Teacher_Sidebar";
-import HelpHome2 from "./components/FileGraveyard/help-home2";
 import HelpHome from "./components/Help_Page/HelpHome";
+import HelpStudents from "./components/Help_Page/HelpStudents";
+import {MantineProvider} from "@mantine/core";
 
 function App() {
     return(
@@ -19,8 +19,8 @@ function App() {
               <Route path='/login' element={<LoginWindow/>}/>
               <Route path='/start' element={<StartPage/>}/>
               <Route path='/ts' element={<Teacher_Sidebar/>}/>
-              <Route path='/help' element={<HelpHome2/>}/>
-              <Route path='/help2' element={<HelpHome/>}/>
+              <Route path='/help' element={<HelpHome/>}/>
+              <Route path='/help/student' element={<HelpStudents/>}/>
           </Routes>
         </Router>
     );
