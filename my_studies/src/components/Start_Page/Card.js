e80f4e81cf8c0e15d8e45d1a17137a6de8379fe0
Konstantinos Leontiadis <123456kost@gmail.com>
2024-01-12 00:09:17 +0200

    things to be done tomorrow

--- my_studies/src/components/Start_Page/Card.js
+++ my_studies/src/components/Start_Page/Card.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Card.css';
+import {BiCard} from "react-icons/bi";
 
-function Card({text, image, image_alt, data_value}) {
+function Card({text, image, image_alt, data_value, icon}) {
+
     return (
         <div className={'card_outer'} data-value={data_value}>
             <img src={image} alt={image_alt} className={'image_opaque'}/>
             <div className={'text_in_card'}>{text}</div>
+            <div className={'icon_in_card'}>
+                {icon}
+            </div>
         </div>
     );
 }
--- my_studies/src/components/Start_Page/ContentCarousel.js
+++ my_studies/src/components/Start_Page/ContentCarousel.js
@@ -4,6 +4,8 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import Announcement from "./Announcement";
 import './ContentCarousel.css';
 import Card from "./Card";
+import {BiCard} from "react-icons/bi";
+import {AiFillLock} from "react-icons/ai";
 // import React from 'react';
 
 const responsive = {
@@ -13,8 +15,8 @@ const responsive = {
 };
 
 const items = [
-    <Card text={'FirstCardodfsfjiofjdsfioljdsfioldsjfioldsjflikdsfjdslkfjdsflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={1}/>,
-    <Card text={'FirstCardodfsfjiofjdsfioljdsfioldsjfioldsjflikdsfjdslkfjdsflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={2}/>,
+    <Card text={'FirstCardodfsfjiofjdsfioljdsfioldsjfioldsjflikdsfjdslkfjdsflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={1} icon={<BiCard/>}/>,
+    <Card text={'FirstCardodfsfjiofjdsfioljdsfioldsjfioldsjflikdsfjdslkfjdsflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={2} icon={<AiFillLock/>}/>,
     <div data-value="3">3</div>,
     <div data-value="4">4</div>,
     <div data-value="5">5</div>,
