563e7d279f59d24fca5daf9bef2205ec0de35419
Konstantinos Leontiadis <123456kost@gmail.com>
2024-01-13 13:30:19 +0200

    filled teacher carousel

--- my_studies/src/components/Start_Page/StartPage.js
+++ my_studies/src/components/Start_Page/StartPage.js
@@ -12,6 +12,8 @@ import {FaScroll} from "react-icons/fa6";
 import {BsBookmarkPlus, BsBookmarkPlusFill} from "react-icons/bs";
 import {MdSettings} from "react-icons/md";
 import {IoHelpCircleOutline} from "react-icons/io5";
+import {FaUserCircle} from "react-icons/fa";
+import {GiScrollQuill} from "react-icons/gi";
 
 const items_1 = [
     <Card text={'Δες πληροφορίες για τα μαθήματα που έχεις πάρει, αλλά και όσα μπορείς να πάρεις. Δες πληροφορίες όπως ποιοί καθηγητές τα έχουν πάρει, πόσες πιστωτικές μονάδες δίνουν και πολλά άλλα.'} image={'/carousel/student/lessons.jpg'} image_alt={'lessons'} data_value={1} icon={<FaBook/>} title={'Μαθήματα'} type={"student"}/>,
@@ -19,15 +21,16 @@ const items_1 = [
     <Card text={'Δήλωσε τα μαθήματα που θες να παρακολουθήσεις! Μπορείς να κάνεις όσες δηλώσεις θες εντός της προθεσμίας άμα αλλάξεις την γνώμη σου!'} image={'/carousel/student/dhloseis.png'} image_alt={'dhloseis'} data_value={3} icon={<BsBookmarkPlusFill/>}  title={'Δηλώσεις'} type={"student"}/>,
     <Card text={'Έκδοσε πιστοποιητικά από τα διαθέσιμα είδη. Μπορείς ανά πάσα στιγμή να ελέγξεις την κατάσταση τους και να εκτυπώσεις όσα έχουν εγκριθεί'} image={'/carousel/student/certificate.jpg'} image_alt={'certificate'} data_value={4} icon={<FaCertificate/>}  title={'Πιστοποιητικά'} type={"student"}/>,
     <Card text={'Εξερεύνησε τις ρυθμίσεις που προσφέρει η εφαρμογή μας. Προσάρμοσε την στις ανάγκες σου!'} image={'/carousel/student/settings.jpg'} image_alt={'settings'} data_value={5} icon={<MdSettings/>}  title={'Ρυθμίσεις'} type={"student"}/>,
-    <Card text={'Η δικιά σου σελίδα βοήθειας για να εξερευνήσεις τις δυνατότητες της εφαρμογής με ασφάλεια!'} image={'/carousel/student/help.jpg'} image_alt={'help'} data_value={6} icon={<IoHelpCircleOutline/>}  title={'Βοήθεια'} type={"student"}/>
+    <Card text={'Η δικιά σου σελίδα βοήθειας για να εξερευνήσεις τις δυνατότητες της εφαρμογής με ασφάλεια!'} image={'/carousel/student/help.jpg'} image_alt={'help'} data_value={6} icon={<IoHelpCircleOutline/>}  title={'Βοήθεια'} type={"student"}/>,
+    <Card text={'Προσάρμοσε το προφίλ σου και κάντο να ξεχωρίζει! Δες τις πληροφορίες σου, επεξεργάσου τες και αποθήκευσέ τες!'} image={'/carousel/student/profile.jpg'} image_alt={'profile'} data_value={7} icon={<FaUserCircle/>}  title={'Προφίλ'} type={"student"}/>
 ];
 
 const items_2 = [
-    <Card text={'First Cardodfsfjiofjdsfio ljdsfioldsjfioldsjflikdsfjdslkfjdsdsadsadsadsasflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={1} icon={<BiCard/>} title={'Δες Βαθμούς'} type={"teacher"}/>,
-    <Card text={'First Cardodfsfjiofjdsfiol jdsfioldsjfioldsjflikdsfjdsssdsadsadsadlkfjdsflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={2} icon={<AiFillLock/>}  title={'Δες Βαθμούς'} type={"teacher"}/>,
-    <Card text={'First Cardodfsfjiofjdsfiol jdsfioldsjfioldsjflikdsfjdsssdsadsadsadlkfjdsflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={3} icon={<AiFillLock/>}  title={'Δες Βαθμούς'} type={"teacher"}/>,
-    <Card text={'First Cardodfsfjiofjdsfiol jdsfioldsjfioldsjflikdsfjdsssdsadsadsadlkfjdsflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={4} icon={<AiFillLock/>}  title={'Δες Βαθμούς'} type={"teacher"}/>,
-    <Card text={'First Cardodfsfjiofjdsfiol jdsfioldsjfioldsjflikdsfjdsssdsadsadsadlkfjdsflkjd'} image={'/bookcase.jpg'} image_alt={'bookcase'} data_value={5} icon={<AiFillLock/>}  title={'Δες Βαθμούς'} type={"teacher"}/>
+    <Card text={'Δες τα μαθήματα που έχεις αναλάβει και τις πληροφορίες τους! Δες τις βαθμολογίες που έχεις ανεβάσει και εάν έχεις ανεβάσει!'} image={'/carousel/teacher/lessons.jpg'} image_alt={'lessons'} data_value={1} icon={<FaBook/>} title={'Μαθήματα'} type={"teacher"}/>,
+    <Card text={'Δημιούργησε ένα νέο βαθμολόγιο! Ανέβασε βαθμολογίες μαζικά ή μεμονομένα και είτε αποθηκευσέ τες προσωρινά είτε οριστικοποίησέ τες!'} image={'/carousel/teacher/grades.jpg'} image_alt={'grades'} data_value={2} icon={<GiScrollQuill/>}  title={'Βαθμολόγια'} type={"teacher"}/>,
+    <Card text={'Εξερεύνησε τις ρυθμίσεις που προσφέρει η εφαρμογή μας. Προσάρμοσε την στις ανάγκες σου!'} image={'/carousel/student/settings.jpg'} image_alt={'settings'} data_value={3} icon={<MdSettings/>}  title={'Ρυθμίσεις'} type={"teacher"}/>,
+    <Card text={'Η δικιά σου σελίδα βοήθειας για να εξερευνήσεις τις δυνατότητες της εφαρμογής με ασφάλεια!'} image={'/carousel/student/help.jpg'} image_alt={'help'} data_value={4} icon={<IoHelpCircleOutline/>}  title={'Βοήθεια'} type={"teacher"}/>,
+    <Card text={'Προσάρμοσε το προφίλ σου και κάντο να ξεχωρίζει! Δες τις πληροφορίες σου, επεξεργάσου τες και αποθήκευσέ τες!'} image={'/carousel/student/profile.jpg'} image_alt={'profile'} data_value={5} icon={<FaUserCircle/>}  title={'Προφίλ'} type={"teacher"}/>
 ];
 
 const items_3 = [
