88254ba5de88425afe353e1bfd8989101f6dc304
Konstantinos Leontiadis <123456kost@gmail.com>
2024-01-16 18:18:11 +0200

    need to fix and add elements

--- my_studies/src/components/Start_Page/StartPageHeader.js
+++ my_studies/src/components/Start_Page/StartPageHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './StartPageHeader.css';
+import UniversalNavbar from "../Navbar/UniversalNavbar";
 
 function StartPageHeader(props) {
     return (
-        <div>
+        <div className={'out_div'}>
             <img src={'/logo.png'} alt={"Logo"} className={'logo_ekpa'}/>
+            <UniversalNavbar/>
         </div>
     );
 }
