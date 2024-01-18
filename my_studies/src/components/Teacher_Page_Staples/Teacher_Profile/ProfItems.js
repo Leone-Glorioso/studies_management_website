7094a0883f22f148161dccd8848c160be4c4c84d
sdi2100127 <sdi2100127@di.uoa.gr>
2024-01-18 21:12:30 +0200

    teacher profile

---
+++ my_studies/src/components/Teacher_Page_Staples/Teacher_Profile/ProfItems.js
@@ -0,0 +1,84 @@
+export const Prof1 = [
+    {
+        cName: "prof-info",
+        title: "Ημερομηνία πρώτης εγγραφής"
+    },
+    {
+        cName: "prof-info",
+        title: "Εξάμηνο φοίτησης"
+    },
+    {
+        cName: "prof-info",
+        title: "Εξάμηνο πρώτης εγγραφής"
+    },
+    {
+        cName: "prof-info",
+        title: "Ακαδημαϊκό έτος"
+    },
+]
+
+export const Prof2 = [
+    {
+        cName: "prof-info",
+        title: "Όνομα",
+    },
+    {
+        cName: "prof-info",
+        title: "Επίθετο",
+    },
+    {
+        cName: "prof-info",
+        title: "Όνομα πατέρα"
+    },
+    {
+        cName: "prof-info",
+        title: "Όνομα μητέρας"
+    },
+    {
+        cName: "prof-info",
+        title: "Ημερομηνία γέννησης"
+    },
+    {
+        cName: "prof-info",
+        title: "Οικογενειακή κατάσταση"
+    },
+]
+
+export const Prof3 = [
+    {
+        cName: "prof-info",
+        title: "Μόνιμη διεύθυνση κατοικίας"
+    },
+    {
+        cName: "prof-info",
+        title: "Μόνιμη πόλη κατοικίας"
+    },
+    {
+        cName: "prof-info",
+        title: "ΤΚ μόνιμης κατοικίας"
+    },
+    {
+        cName: "prof-info",
+        title: "Τηλέφωνο μόνιμης κατοικίας"
+    },
+    {
+        cName: "prof-info",
+        title: "Προσωρινή διεύθυνση κατοικίας"
+    },
+    {
+        cName: "prof-info",
+        title: "Προσωρινή πόλη κατοικίας"
+    },
+    {
+        cName: "prof-info",
+        title: "ΤΚ προσωρινής κατοικίας"
+    },
+    {
+        cName: "prof-info",
+        title: "Τηλέφωνο προσωρινής κατοικίας"
+    },
+    {
+        cName: "prof-info",
+        title: "Διεύθυνση ηλεκτρονικού ταχυδρομείου"
+    },
+]
