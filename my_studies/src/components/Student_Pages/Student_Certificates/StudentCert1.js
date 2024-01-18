9bb8a725fa331053369189272acb8f1a798710bd
Konstantinos Leontiadis <123456kost@gmail.com>
2024-01-18 16:02:53 +0200

    first stage of certs done

--- my_studies/src/components/Student_Pages/Student_Certificates/StudentCert1.js
+++ my_studies/src/components/Student_Pages/Student_Certificates/StudentCert1.js
@@ -1,60 +1,70 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import './StudentCert1.css'
 import Sidebar from "../Navbar_Sidebar/Sidebar";
+import {useAuth} from "../../Auth/AuthContext";
 
-class StudentCert1 extends Component {
+function StudentCert1() {
 
-    render() {
-        return (
-            <div>
-                <Sidebar/>
+    const Auth = useAuth();
+    const [type, SetType] = useState(0);
 
-                <ul className="breadcrumb">
-                    <li><a href="/student">Αρχική Μαθητή</a></li>
-                    <li><a href="/student/certificates">Πιστοποιητικά</a></li>
-                    <li>Νέο Πιστοποιητικό</li>
-                </ul>
+    const handleRadioButton = (num) => {
+        SetType(num);
+    }
 
-                <ul className="progress-bar">
-                    <li className="table-header">
-                        <div className="step1">Επιλογή Πιστοποιητικού</div>
-                        <div className="step2">Έλεγχος Στοιχείων</div>
-                        <div className="step3">Επιβεβαίωση Τελικής δήλωσης</div>
-                        <div className="step4">Επιβεβαίωση ολοκλήρωσης</div>
-                    </li>
-                </ul>
+    const onContinue = (num) => {
+        Auth.setType(num);
+    }
 
-                <ul className="progress-bar1">
-                    <li className="table-header">
-                        <div className="step1">1</div>
-                        <div className="step2">2</div>
-                        <div className="step3">3</div>
-                        <div className="step4">4</div>
-                    </li>
-                </ul>
+    return (
+        <div>
+            <Sidebar/>
 
-                <p className="text">Επέλεξε τον τύπο του πιστοποιητικού που θέλεις να εκδόσεις:</p>
+            <ul className="breadcrumb">
+                <li><a href="/student">Αρχική Μαθητή</a></li>
+                <li><a href="/student/certificates">Πιστοποιητικά</a></li>
+                <li>Νέο Πιστοποιητικό</li>
+            </ul>
 
-                <div className="radio-buttons">
-                    <label><input type="radio" name="e"/> Φοιτητικής Ιδιότητας</label>
-                    <label><input type="radio" name="e"/> Φορολογικής Χρήσης</label>
-                    <label><input type="radio" name="e"/> Αναλυτικής βαθμολογίας με προβιβάσιμους</label>
-                    <label><input type="radio" name="e"/> Στρατολογικής Χρήσης (Συνοπτικό)</label>
-                    <label><input type="radio" name="e"/> Στρατολογικής Χρήσης (Αναλυτικό)</label>
-                </div>
+            <ul className="progress-bar">
+                <li className="table-header">
+                    <div className="step1">Επιλογή Πιστοποιητικού</div>
+                    <div className="step2">Έλεγχος Στοιχείων</div>
+                    <div className="step3">Επιβεβαίωση Τελικής δήλωσης</div>
+                    <div className="step4">Επιβεβαίωση ολοκλήρωσης</div>
+                </li>
+            </ul>
 
-                <ul className="buttons1_1">
-                    <li className="buttons-c">
-                        <a href="/student/certificates/" className="prev">Προηγούμενο</a>
-                        <a href="/student/certificates/" className="cancel">Άκυρο</a>
-                        <a href="/student/certificates/new-certificate/personal_info" className="next">Επόμενο</a>
-                    </li>
-                </ul>
+            <ul className="progress-bar1">
+                <li className="table-header">
+                    <div className="step1">1</div>
+                    <div className="step2">2</div>
+                    <div className="step3">3</div>
+                    <div className="step4">4</div>
+                </li>
+            </ul>
 
+            <p className="text">Επέλεξε τον τύπο του πιστοποιητικού που θέλεις να εκδόσεις:</p>
+
+            <div className="radio-buttons">
+                <label><input type="radio" name="e" onChange={() => handleRadioButton(1)}/> Φοιτητικής Ιδιότητας</label>
+                <label><input type="radio" name="e" onChange={() => handleRadioButton(2)}/> Φορολογικής Χρήσης</label>
+                <label><input type="radio" name="e" onChange={() => handleRadioButton(3)}/> Αναλυτικής βαθμολογίας με προβιβάσιμους</label>
+                <label><input type="radio" name="e" onChange={() => handleRadioButton(4)}/> Στρατολογικής Χρήσης (Συνοπτικό)</label>
+                <label><input type="radio" name="e" onChange={() => handleRadioButton(5)}/> Στρατολογικής Χρήσης (Αναλυτικό)</label>
             </div>
 
-        )
-    }
+            <ul className="buttons1_1">
+                <li className="buttons-c">
+                    <a href="/student/certificates/" className="prev">Προηγούμενο</a>
+                    <a href="/student/certificates/" className="cancel">Άκυρο</a>
+                    <a href="/student/certificates/new-certificate/personal_info" className="next" onClick={onContinue(type)}>Επόμενο</a>
+                </li>
+            </ul>
+
+        </div>
+
+    )
 
 }
 
