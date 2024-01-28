7a2c50c87e957f896a455ad5c715b99e4ff1fc53
Konstantinos Leontiadis <123456kost@gmail.com>
2024-01-28 02:47:57 +0200

    see old dhloseis

--- my_studies/src/components/Student_Pages/Student_Dhlwseis/StudentDhlwseis4.js
+++ my_studies/src/components/Student_Pages/Student_Dhlwseis/StudentDhlwseis4.js
@@ -2,7 +2,7 @@ import React, {Component, useEffect, useState} from "react";
 import './StudentDhlwseis4.css'
 import Sidebar from "../Navbar_Sidebar/Sidebar";
 import {useAuth} from "../../Auth/AuthContext";
-import {collection, getDocs, orderBy, query, where} from "firebase/firestore";
+import {collection, getDocs, orderBy, query, where, doc, getDoc} from "firebase/firestore";
 import {db} from "../../config/firebase_config";
 
 function StudentDhlwseis4() {
@@ -12,8 +12,8 @@ function StudentDhlwseis4() {
     const user = Auth.getUser();
     const [comps, setComps] = useState([]);
     // const [active, setActive] = useState(false);
-    // const [lessons_in, setLessonsIn] = useState([]);
-    // const [lessons_out, setLessonsOut] = useState([]);
+    const [lessons_in, setLessonsIn] = useState([]);
+    const [lessons_out, setLessonsOut] = useState([]);
     // const [pos, setPos] = useState(0);
     // let pos = 0;
     // let lessons_in = [];
@@ -23,47 +23,38 @@ function StudentDhlwseis4() {
         async function fetdhDhl()
         {
             const db_ref = collection(db, 'dhloseis');
-            const q = query(db_ref, where('student_username', '==', user.username), where('type', '==', 'final'), orderBy("date", "desc"));
+            const q = query(db_ref, where('student_username', '==', user.username), where('type', '==', 'final') , where('isCurrent', '==', false), orderBy("date", "desc"));
             const docs = await getDocs(q);
             const data = [];
             docs.forEach((doc)=> {
-                data.push({date: doc.data().date.toDate().toDateString(), time: doc.data().date.toDate().toLocaleTimeString("en-GB")});
+                data.push({date: doc.data().date.toDate().toDateString(), time: doc.data().date.toDate().toLocaleTimeString("en-GB"), id:doc.id});
             })
+            console.log(data);
             setComps(data);
         }
-        if(isLogged)
+        if(isLogged && user.type === 'student')
             fetdhDhl();
     }, []);
 
-    async function onClickView(e, pos) {
+    async function onClickView(e, id) {
         async function fetchLessons()
         {
-            Auth.setLessonsIn([]);
-            Auth.setLessonsOut([]);
-            const db_ref = collection(db, 'dhloseis');
-            const q = query(db_ref, where('student_username', '==', user.username), where('type', '==', 'final'), orderBy("date", "desc"));
-            const docs = await getDocs(q);
-            const data = [];
-            docs.forEach((doc)=> {
-                data.push(doc.data().lessons);
-            })
-            console.log(data, data[pos], pos);
-            const in_dhloseis = data[pos];
-            console.log(in_dhloseis);
+            const doc_ref = doc(db, 'dhloseis', id);
+            const doc_ret = await getDoc(doc_ref);
+            const in_dhloseis = doc_ret.data().lessons;
             const db_ref_alt = collection(db, 'lessons');
             const q_alt = query(db_ref_alt);
             const docs_alt = await getDocs(q_alt);
             const less_in = [];
             const less_out = [];
             docs_alt.forEach((doc)=> {
-                console.log(doc.data().num);
-                if(doc.data().num in in_dhloseis)
+                if(in_dhloseis.includes(doc.data().num))
                     less_in.push({id: doc.id, ...doc.data()});
                 else
                     less_out.push({id: doc.id, ...doc.data()});
             })
-            Auth.setLessonsIn(less_in);
-            Auth.setLessonsOut(less_out);
+            setLessonsIn(less_in);
+            setLessonsOut(less_out);
         }
         if(isLogged && user.type === 'student'){
             await fetchLessons();
@@ -108,12 +99,12 @@ function StudentDhlwseis4() {
                         <div className="col col-2">Ώρα</div>
                         <div className="col col-3"></div>
                     </li>
-                    {comps.map((comp,index) => {
+                    {comps.map((comp) => {
                         return (
                             <li className="table-row">
                                 <div className="col col-1" data-label="date">{comp.date}</div>
                                 <div className="col col-2" data-label="type">{comp.time}</div>
-                                <a id={index} href="#popup-ep" className="col col-3" onClick={(e) => onClickView(e, index)}>Προβολή</a>
+                                <a id={comp.id} href="#popup-ep" className="col col-3" onClick={(e)=> onClickView(e, comp.id)}>Προβολή</a>
                             </li>
                         );
                     })}
@@ -175,7 +166,7 @@ function StudentDhlwseis4() {
                                 <th>Τίτλος Μαθήματος</th>
                                 <th>Δήλωση</th>
                             </tr>
-                            {Auth.getLessonsIn().map((item) => {
+                            {lessons_in.map((item) => {
                                 return (
                                     <tr>
                                         <td>{item.num}</td>
@@ -185,7 +176,7 @@ function StudentDhlwseis4() {
                                 )
                             })}
 
-                            {Auth.getLessonsOut().map((item) => {
+                            {lessons_out.map((item) => {
                                 return (
                                     <tr>
                                         <td>{item.num}</td>
